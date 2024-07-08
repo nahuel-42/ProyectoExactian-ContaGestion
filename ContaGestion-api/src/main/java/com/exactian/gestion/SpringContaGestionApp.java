8c37b911b963e744fbc516c49a91f7bf76b37ba9
nahuel-42 <nievas.nahuel.1998@gmail.com>
2024-07-08 16:31:32 -0300

    feature: Definicion de directorios, estructura de backend y conexion a base de datos PostgresSQL

---
+++ ContaGestion-api/src/main/java/com/exactian/gestion/SpringContaGestionApp.java
@@ -0,0 +1,13 @@
+package com.exactian.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContaGestionApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContaGestionApp.class, args);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ProyectoExactian
 Aplicación orientada al registro de los ingresos y egresos de los empleados en una compañía
+
+Link a documentacion completa:
+https://docs.google.com/document/d/1QqkmbGu-QVaXu2WTvxjEgf8ciAXYl4GGAoUJX3RiU6U/edit#heading=h.qm302omykudn
